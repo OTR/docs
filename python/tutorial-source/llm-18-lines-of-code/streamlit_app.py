272e1c76c340c8cac269a3d61c264fa399942d2e
Andrew Popkov <49165533+OTR@users.noreply.github.com>
2024-12-05 14:54:10 +0000

    title

--- python/tutorial-source/llm-18-lines-of-code/streamlit_app.py
+++ python/tutorial-source/llm-18-lines-of-code/streamlit_app.py
@@ -1,13 +1,23 @@
 import streamlit as st
 from langchain_openai.chat_models import ChatOpenAI
 
+st.set_page_config(
+        page_title="Base of Bot",
+)
 st.title("🤖 Base of Bot")
 
 openai_api_key = st.secrets["key"]
 
 
 def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+    model = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        api_key=openai_api_key
+    )
     resp = model.invoke(input_text)
     st.info(resp.content)
 
